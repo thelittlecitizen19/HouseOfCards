a9db907a2a2aba36608ab8cd985c3995e7aa52fe
thelittlecitizen19 <thelittlecitizen19@gmail.com>
2020-08-31 11:42:38 +0300

    added menu

---
+++ HouseOfCards/Game/GameManeger.cs
@@ -0,0 +1,17 @@
+﻿using HouseOfCards.Players;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HouseOfCards.Game
+{
+    public class GameManeger
+    {
+        public Game GamBoard { get; set; }
+        public Menu Menu { get; set; }
+        public GameManeger(List<Player> players)
+        {
+            GamBoard = new Game(players);
+        }
+    }
+}
---
+++ HouseOfCards/Menu/Menu.cs
@@ -0,0 +1,33 @@
+﻿using HouseOfCards.Displays;
+using HouseOfCards.Players;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HouseOfCards.Menu
+{
+    public class Menu
+    {
+        private IDisplay _displayer;
+        public Dictionary<string, Action> Operations { get; set; }
+        public Menu(IDisplay displayer)
+        {
+            Operations = new Dictionary<string, Action>();
+            _displayer = displayer;
+            
+        }
+        public void LoadOperations(Player player)
+        {
+            Operations.Clear();
+            Operations.Add("1", player.PlayCard);
+            Operations.Add("2", player.GetHint);
+        }
+        public void DisplayMenu()
+        {
+            _displayer.Print("for play card press 1");
+            _displayer.Print("for get hint press 2");
+            _displayer.Print("for throw card press 3");
+        }
+
+    }
+}
