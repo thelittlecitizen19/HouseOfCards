5ece7ea8e124fa955d9ce3514f0e2d641d3a7a52
thelittlecitizen19 <thelittlecitizen19@gmail.com>
2020-08-31 09:19:20 +0300

    add CardType

---
+++ HouseOfCards/Cards/CardType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HouseOfCards.Cards
+{
+    public enum CardType
+    {
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        White
+    }
+}
